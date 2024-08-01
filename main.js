1dcd0c73cb4dc1e9cb0663a99a115e567ee095a2
sonia <soniayjw@gmail.com>
2024-08-02 00:11:08 +0800

    upload index.html

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 import './assets/scss/all.scss';
 
-console.log("Hello world!");
+$(document).ready(function () {
+  $('.tab-item').on('click', function (){
+    let target = $(this).data('target');
+
+    $('.tab-item').removeClass('active');
+    $('.tab-content').removeClass('active');
+
+    $(this).addClass('active');
+    $(target).addClass('active');
+  })
+
+
+});
